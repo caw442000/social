d3194ebb586b7d8585a25854b4ec9d5f835733d7
cedric-winbush <cawinbushjr@gmail.com>
2020-11-03 17:01:51 -0600

    cleans up file structure

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const typeDefs = require ('./graphql/typeDefs.js');
+const resolvers = require ('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-const typeDefs = gql `
-  type Query {
-      sayHi: String!
-  }
-
-`
-
-const resolvers = {
-  Query: {
-    sayHi: () => ' Hello World'
-  }
-}
 
 
 const server = new ApolloServer({
