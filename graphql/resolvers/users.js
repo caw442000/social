047bccfa151f415d71ae6fef46a28339ffd0e6a7
cedric-winbush <cawinbushjr@gmail.com>
2020-11-09 17:12:35 -0600

    adds get post

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -9,7 +9,7 @@ const { SECRET_KEY } = require('../../config.js')
 const User = require('../../models/User');
 
 function generateToken(user) {
- return jwt.sign({
+  return jwt.sign({
     id: user.id, 
     email: user.email, 
     username: user.username
